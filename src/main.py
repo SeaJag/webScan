d8e9fe8be3b5bb5e5812af7659d795cbf6c5fbcc
abdelouahid.behar@epita.fr <SeaJag>
2023-04-15 12:41:31 -0400

    rapport des failles détéctées

--- src/main.py
+++ src/main.py
@@ -6,6 +6,9 @@ session = login()
 toCheck = 'http://172.17.0.1' + '/'
 url_list = get_url_list(session, toCheck)
 
+print("\n===========================   Liste des pages à tester   =======================================")
 print(url_list)
 
+print("\n===========================   Rapport des failles détectées   =======================================")
 check_vulnerabilities(session, url_list)
+print("================================       Fin du Rapport       =========================================")
